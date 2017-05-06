1f1a0484ae4f510fb019e0873fc1fc192d46f0fd
Marissa Pelipas <marissa.tayaopelipas@gmail.com>
2017-05-06 15:04:27 -0400

    Add files via upload
    Weather App source code

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+import {AppComponent} from './app.component';
+import {WeatherComponent} from './weather/weather.component';
+import {AppRoutingModule} from "./app-routing.module";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WeatherComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
